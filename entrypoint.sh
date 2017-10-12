25405b4a77f28f319fb95f6f531e79367cad0804
MChen <ming.chen0919@gmail.com>
2017-10-12 00:36:09 -0400

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,13 @@ echo "starting Galaxy Tool Generator..."
 echo
 sleep 5
 echo
-echo "Galaxy Tool Generator is ready at http://127.0.0.1:8080/"
+echo "Galaxy Tool Generator is ready at http://127.0.0.1:8090/"
 echo
+
+## starting galaxy
+#sh /root/galaxy/run.sh --daemon
+#echo 'starting galaxy'
+#sleep 60
 
 ##
 sh -c "$@"
